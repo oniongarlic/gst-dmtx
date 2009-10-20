c5fc65ca27d4354fceb12a80235b74bff43162e3
Kaj-Michael Lang <milang@tal.org>
2009-10-20 14:00:00 +0300

    Adjust test script a bit

--- gst-dmtx-test-pipe.sh
+++ gst-dmtx-test-pipe.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-gst-launch --gst-plugin-path=`pwd`/src/.libs v4l2src ! video/x-raw-rgb,width=320, height=240 ! dmtx ! ffmpegcolorspace ! xvimagesink
+gst-launch --gst-plugin-path=`pwd`/src/.libs \
+	v4l2src ! video/x-raw-rgb,width=320, height=240,fps=10 ! \
+	dmtx scale=2 ! \
+	queue ! ffmpegcolorspace ! xvimagesink
