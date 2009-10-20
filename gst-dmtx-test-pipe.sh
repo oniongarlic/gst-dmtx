7a3723fd284198eeed6d2748fb1259d841482f54
Kaj-Michael Lang <milang@tal.org>
2009-10-20 14:04:33 +0300

    Add image test case, adjust test script

--- gst-dmtx-test-pipe.sh
+++ gst-dmtx-test-pipe.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+case "$1" in
+video)
 gst-launch --gst-plugin-path=`pwd`/src/.libs \
 	v4l2src ! video/x-raw-rgb,width=320, height=240,fps=10 ! \
 	dmtx scale=2 ! \
 	queue ! ffmpegcolorspace ! xvimagesink
+;;
+png)
+gst-launch --gst-plugin-path=`pwd`/src/.libs filesrc location=test/test.png ! pngdec ! dmtx ! fakesink
+;;
+*)
+echo "Usage: $0 png|video"
+;;
+esac
