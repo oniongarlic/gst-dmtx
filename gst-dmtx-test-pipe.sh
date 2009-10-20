c2add8c47ff5bead772e57153ddf89f56ede5169
Kaj-Michael Lang <milang@tal.org>
2009-10-20 15:18:24 +0300

    Show messages

--- gst-dmtx-test-pipe.sh
+++ gst-dmtx-test-pipe.sh
@@ -2,19 +2,19 @@
 
 case "$1" in
 video)
-gst-launch --gst-plugin-path=`pwd`/src/.libs \
+gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
 	v4l2src ! video/x-raw-rgb,width=320, height=240,fps=10 ! \
-	dmtx scale=2 timeout=100 ! \
+	dmtx scale=2 timeout=20 ! \
 	queue ! ffmpegcolorspace ! xvimagesink
 ;;
 video-eos)
-gst-launch --gst-plugin-path=`pwd`/src/.libs \
+gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
 	v4l2src ! video/x-raw-rgb,width=320, height=240,fps=2 ! \
-	dmtx scale=3 stop_after=1 timeout=50 ! \
+	dmtx scale=3 stop_after=1 timeout=10 ! \
 	queue ! ffmpegcolorspace ! xvimagesink
 ;;
 png)
-gst-launch --gst-plugin-path=`pwd`/src/.libs filesrc location=test/test.png ! pngdec ! dmtx ! fakesink
+gst-launch -m --gst-plugin-path=`pwd`/src/.libs filesrc location=test/test.png ! pngdec ! dmtx ! fakesink
 ;;
 *)
 echo "Usage: $0 png|video|video-eos"
