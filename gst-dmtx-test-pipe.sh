79d8e2cbc3401cff59e8882dfc43e2b60723b2cd
Kaj-Michael Lang <milang@tal.org>
2009-10-28 17:12:46 +0200

    Add test cases for 16-bit and 8-bit input

--- gst-dmtx-test-pipe.sh
+++ gst-dmtx-test-pipe.sh
@@ -19,6 +19,18 @@ gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
 	tee name=t ! queue ! ffmpegcolorspace ! \
 	dmtx scale=2 timeout=100 skip=30 skip_dups=TRUE ! fakesink t. ! queue ! ffmpegcolorspace ! xvimagesink
 ;;
+video-16)
+gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
+	v4l2src ! ffmpegcolorspace ! video/x-raw-rgb,width=320,height=240 ! \
+	tee name=t ! queue ! ffmpegcolorspace ! video/x-raw-rgb,width=320,height=240,bpp=16 ! \
+	dmtx scale=2 timeout=100 skip=30 skip_dups=TRUE ! fakesink t. ! queue ! ffmpegcolorspace ! xvimagesink
+;;
+video-8)
+gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
+	v4l2src ! ffmpegcolorspace !video/x-raw-rgb,width=320,height=240 ! \
+	tee name=t ! queue ! ffmpegcolorspace ! video/x-raw-gray,width=320,height=240,bpp=8 ! \
+	dmtx scale=2 timeout=100 skip=30 skip_dups=TRUE ! fakesink t. ! queue ! ffmpegcolorspace ! xvimagesink
+;;
 png)
 gst-launch -m --gst-plugin-path=`pwd`/src/.libs filesrc location=test/test.png ! pngdec ! dmtx ! fakesink
 ;;
