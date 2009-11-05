862de5ac159e6c9f3092a8569c35116403a5610c
Kaj-Michael Lang <milang@tal.org>
2009-11-05 17:17:58 +0200

    Version 0.10.2

--- gst-dmtx-test-pipe.sh
+++ gst-dmtx-test-pipe.sh
@@ -27,8 +27,8 @@ gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
 ;;
 video-8)
 gst-launch -m --gst-plugin-path=`pwd`/src/.libs \
-	v4l2src ! ffmpegcolorspace !video/x-raw-rgb,width=320,height=240 ! \
-	tee name=t ! queue ! ffmpegcolorspace ! video/x-raw-gray,width=320,height=240,bpp=8 ! \
+	v4l2src ! ffmpegcolorspace !video/x-raw-rgb,width=640,height=480 ! \
+	tee name=t ! queue ! ffmpegcolorspace ! video/x-raw-gray,width=640,height=480,bpp=8 ! \
 	dmtx scale=2 timeout=100 skip=30 skip_dups=TRUE qos=TRUE ! fakesink t. ! queue ! ffmpegcolorspace ! xvimagesink
 ;;
 png)
